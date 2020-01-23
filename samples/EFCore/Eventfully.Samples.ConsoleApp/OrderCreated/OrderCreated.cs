a778c3b5530876a6a8bd7f24dc7e001572f5567f
cfrenzel <camron.frenzel@bleepworks.com>
2020-01-23 11:57:13 -0500

    updated sample

--- samples/EFCore/Eventfully.Samples.ConsoleApp/OrderCreated/OrderCreated.cs
+++ samples/EFCore/Eventfully.Samples.ConsoleApp/OrderCreated/OrderCreated.cs
@@ -28,7 +28,7 @@ namespace Eventfully.Samples.ConsoleApp
         {
             public string Line1 { get; private set; }
             public string Line2 { get; private set; }
-                public string City { get; private set; }
+            public string City { get; private set; }
             public string StateCode { get; private set; }
             public string Zip { get; private set; }
 
