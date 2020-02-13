3dbdccce188c8acc8f794649440924f8f8f969f2
cfrenzel <camron.frenzel@bleepworks.com>
2020-02-13 17:00:12 -0500

    analyzers to add handlers in sagas

--- tests/Eventfully.Core.Analyzers.Test/MissingMapIdForAnalyzerTests.cs
+++ tests/Eventfully.Core.Analyzers.Test/MissingMapIdForAnalyzerTests.cs
@@ -18,7 +18,38 @@ namespace Eventfully.Core.Analyzers.Test
         [TestMethod]
         public void Should_be_no_diagnostics_by_default()
         {
-            var test = @"";
+            var test = @"using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eventfully.Samples.Application;
+using Microsoft.Extensions.Configuration;
+
+namespace Eventfully.Samples.ConsoleApp
+{
+    public class MessagingProfile : Profile
+    {
+        public MessagingProfile(IConfiguration config)
+        {
+            ConfigureEndpoint(""Events"")
+              .AsInboundOutbound()
+                 .UseLocalTransport()
+             ;
+
+            ConfigureEndpoint(""Commands"")
+                .AsInboundOutbound()
+                .UseLocalTransport()
+                ;
+
+            ConfigureEndpoint(""Replies"")
+                .AsInboundOutbound()
+                .AsReplyDefault()
+                .UseLocalTransport()
+                ;
+        }
+    }
+}
+";
             VerifyCSharpDiagnostic(test);
         }
 
@@ -61,7 +92,7 @@ namespace Eventfully.Core.Analyzers.Test
        
             var expected = new DiagnosticResult
             {
-                Id = "EventfullyCoreAnalyzers",
+                Id = "MissingMapIdForAnalyzer",
                 Message = "Missing this.MapIdFor<PizzaOrderedEvent> in constructor",
                 Severity = DiagnosticSeverity.Warning,
                 Locations =
@@ -109,10 +140,99 @@ namespace Eventfully.Core.Analyzers.Test
             VerifyCSharpFix(test, fixtest, null, true);
         }
 
-       
+
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public void Should_fine_missing_handlers_for_mapped_events_in_saga()
+        {
+            var test =
+@"using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Eventfully.Core.Analyzers.Test
+{
+      
+    public class PizzaFulfillmentProcess : ProcessManagerMachine<PizzaFulfillmentStatus, Guid>,
+       ITriggeredBy<PizzaOrderedEvent>,
+       IMachineMessageHandler<PizzaPaidForEvent>,
+       IMachineMessageHandler<PizzaPreparedEvent>
+    {  
+        private readonly ILogger<PizzaFulfillmentProcess> _log;
+        private readonly IMessagingClient _messageClient;
+         
+        public PizzaFulfillmentProcess(IMessagingClient messageClient, ILogger<PizzaFulfillmentProcess> log)
+        { 
+            _log = log;
+            _messageClient = messageClient;
+                     
+            this.MapIdFor<PizzaPaidForEvent>((m, md) => m.OrderId);
+            this.MapIdFor<PizzaPreparedEvent>((m, md) => m.OrderId);
+            this.MapIdFor<PizzaDeliveredEvent>((m, md) => m.OrderId);
+            this.MapIdFor<PizzaOrderedEvent>((m, md) => m.<ProcessId>);
+        }
+    }
+}
+";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "MissingHandlerForMapIdFor",
+                Message = "Missing Handler<PizzaDeliveredEvent> interface",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 26, 18)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+            var fixtest =
+ @"using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Eventfully.Core.Analyzers.Test
+{
+      
+    public class PizzaFulfillmentProcess : ProcessManagerMachine<PizzaFulfillmentStatus, Guid>,
+       ITriggeredBy<PizzaOrderedEvent>,
+       IMachineMessageHandler<PizzaPaidForEvent>,
+       IMachineMessageHandler<PizzaPreparedEvent>,
+       IMachineMessageHandler<PizzaDeliveredEvent>
+    {  
+        private readonly ILogger<PizzaFulfillmentProcess> _log;
+        private readonly IMessagingClient _messageClient;
+         
+        public PizzaFulfillmentProcess(IMessagingClient messageClient, ILogger<PizzaFulfillmentProcess> log)
+        { 
+            _log = log;
+            _messageClient = messageClient;
+                     
+            this.MapIdFor<PizzaPaidForEvent>((m, md) => m.OrderId);
+            this.MapIdFor<PizzaPreparedEvent>((m, md) => m.OrderId);
+            this.MapIdFor<PizzaDeliveredEvent>((m, md) => m.OrderId);
+            this.MapIdFor<PizzaOrderedEvent>((m, md) => m.<ProcessId>);
+        }
+    }
+}
+";
+
+            VerifyCSharpFix(test, fixtest, null, true);
+        }
+
+
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
-            return new MissingMapIdForCodeFixProvider();
+            return new MissingHandlerForMapIdForCodeFixProvider();
         }
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
